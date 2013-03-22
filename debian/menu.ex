7e1aa1048ff1cfc636f8197c6d563ba3d5f8d336
Devon Kearns <dookie@kali.org>
2013-03-22 09:08:56 -0600

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(uniscan):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="uniscan" command="/usr/bin/uniscan"
--- debian/uniscan.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the uniscan package
-#
-0 4	* * *	root	[ -x /usr/bin/uniscan_maintenance ] && /usr/bin/uniscan_maintenance
